ddaab046b9a00495710f7dd6b65cb4f0f3650b01
Renanvkafer <renanvkafer@gmail.com>
2025-09-30 11:05:06 +0800

    Corrigindo pasta de exercícios (não é mais submódulo)

---
+++ Informatica1Python/Esercizi/stub test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import astropy
+import astropy.io.fits as pyfits
+from astropy import constants as const
+print(astropy.__version__)
+print(const.G)
+print(f"luz no vaco: {const.c}")
+
+
+
+
+
