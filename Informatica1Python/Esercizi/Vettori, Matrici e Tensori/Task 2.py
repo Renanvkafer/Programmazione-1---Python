b5313bc80b1aadb848cf117097fa66394524d945
Renanvkafer <renanvkafer@gmail.com>
2025-10-18 16:01:44 +0800

    Func ex vettori e matrici

--- Informatica1Python/Esercizi/Vettori, Matrici e Tensori/Task 2.py
+++ Informatica1Python/Esercizi/Vettori, Matrici e Tensori/Task 2.py
@@ -1,4 +1,147 @@
 """
+Scoprire se la diagonale primaria è nulla: una matrice ha diagonale nulla se tutti gli elementi della diagonale principale sono nulli.
 
+Scoprire se sia la diagonale primaria che la secondaria sono entrant nulle
+
+TIP: per accedere alla diagonale secondaria, invertire l'ordine degli indici
+
+Ripetere l'esercizio utilizzado un ciclo while invece del for
+
+
+Inserire il calcolo in un funzione che restituisca True se la matrice è nulla, False altrimenti e provare per diverse matrici
 
 """
+
+import numpy as np
+
+A = np.array([[0, 3, 3, 0],
+              [-8, 0, 0, 4],
+              [-1, 0, 0, 6],
+              [1, 9, -9, 0]])
+
+diagonale_prima = np.array([])
+
+aux_metta = 0
+
+
+for i in range(len(A)):
+    for j in range(len(A)):
+        if j == aux_metta:
+            diagonale_prima = np.append(diagonale_prima, A[i][j])
+    aux_metta += 1
+
+
+aux_diago = 0
+
+for i in range(len(diagonale_prima)):
+    if diagonale_prima[i] == 0:
+        aux_diago = 1
+    else:
+        aux_diago = 2
+        break
+
+if aux_diago == 1:
+    print(f"La diagonale prima é nulla {diagonale_prima}")
+else:
+    print(f"La diagonale prima non é nulla {diagonale_prima}")
+
+print(f"\nMatrice inteira:\n"
+      f" {A}")
+
+
+aux_metta_inv_i = 0
+aux_metta_inv_j = 0
+
+aux_len = int(len(A)) - 1
+
+
+B = np.array([])
+
+for i in range(len(A)):
+    for j in range(len(A)):
+        B = np.append(B, A[aux_len -  aux_metta_inv_i][aux_len - aux_metta_inv_j])
+        aux_metta_inv_j += 1
+    aux_metta_inv_i += 1
+    aux_metta_inv_j = 0
+
+B = B.reshape(len(A), len(A))
+
+print(f"\nMatrice inversa:\n"
+      f" {B}\n")
+
+
+
+diagonale_secondaria = np.array([])
+
+aux_metta_secondaria = 0
+
+
+for i in range(len(A)):
+    for j in range(len(A)):
+        if j == aux_metta_secondaria:
+            diagonale_secondaria = np.append(diagonale_secondaria, A[i][j])
+    aux_metta += 1
+
+
+aux_diago_secondaria = 0
+
+for i in range(len(diagonale_secondaria)):
+    if diagonale_secondaria[i] == 0:
+        aux_diago_secondaria = 1
+    else:
+        aux_diago_secondaria = 2
+        break
+
+if aux_diago_secondaria == 1:
+    print(f"La diagonale secondaria é nulla {diagonale_secondaria}")
+else:
+    print(f"La diagonale secondaria non é nulla {diagonale_secondaria}")
+
+print(f"\nMatrice inteira:\n"
+      f" {B}")
+
+
+
+
+def trovare_matrice_nulla (matrice):
+    aux_metta_function = 0
+    diagonale_prima_test = np.array([])
+    for a in range(len(matrice)):
+        for b in range(len(matrice)):
+            if b == aux_metta_function:
+                diagonale_prima_test = np.append(diagonale_prima_test, matrice[a][b])
+        aux_metta_function += 1
+
+    aux_diago_func = 0
+
+    print(f"\nMatrice:\n"
+          f"{diagonale_prima_test}\n")
+
+    for a in range(len(matrice)):
+        if diagonale_prima_test[i] == 0:
+            aux_diago_func = 1
+        else:
+            aux_diago_func = 2
+            break
+    if aux_diago_func == 1:
+        return True
+    else:
+        return False
+
+
+matrix = np.array(
+              [[0, 2, 13, 0],
+              [-23, 0, 0, 89],
+              [-28, 0, 0, 102],
+              [1, 9, -323, 0]])
+
+
+if trovare_matrice_nulla(matrix):
+    print("Matrice nulla\n", matrix)
+else:
+    print("Matrice non nulla\n", matrix)
+
+
+
+
+
---
+++ Informatica1Python/Esercizi/Vettori, Matrici e Tensori/Task 3.py
@@ -0,0 +1,121 @@
+"""
+Definiamo due matrici
+
+
+A = np.matrix([[-1,3,0], [1,0,2]]) # prima matrice
+B = np.matrix([[5,2], [7,1], [1,2]]) # seconda matrice
+Calcolare il prodotto matriciale tra le due matrici (senza le funzioni di numpy)
+Sia A∈Rm,p, B∈Rp,n, e C∈Rm,n. La definizione del prodotto riga-colonna può essere espresso come:
+
+cij=aiκbκj
+
+Un elemento di C corrisponde alla molteplicazione tra una riga di A e una colonna di B. L'indice κ serve a scorrere le posizioni delle righe di A e le colonne di B.
+
+Inizializzare C con le dimensioni corrette, prendendole da A e B
+Scrivere in una funzione
+Provare con diverse matrici con dimensione 2x4 e 4x3
+
+Fare lo stesso utilizzando le funzioni di numpy
+
+Calcolare il prodotto punto-punto tra due matrici (senza le funzioni di numpy)
+
+Fare lo stesso utilizzando le liste
+
+Calcolalo con numpy
+
+"""
+import numpy as np
+
+A = np.array([
+    [-1,3,0],
+    [1,0,2]])
+B = np.array([
+    [5,2],
+    [7,1],
+    [1,2]])
+
+
+matrix_produtto = np.array([])
+aux_produtto = 0
+aux_aux = 0
+
+
+for i in range(len(B) - 1):
+    for j in range(len(B) - 1):
+          for b in range(len(A) + 1):
+                aux_aux = A[i][b] * B[b][j]
+                aux_produtto = aux_aux + aux_produtto
+          matrix_produtto = np.append(matrix_produtto, aux_produtto)
+          aux_produtto = 0
+
+
+matrix_produtto = matrix_produtto.reshape(2, 2)
+print(matrix_produtto)
+
+def produtto_matrice (matrice1, matrice2):
+    matrix_produtto_func = np.array([])
+    aux_produtto_func = 0
+    aux_aux_func = 0
+
+    for f in range(len(matrice2) - 1):
+        for l in range(len(matrice2) - 1):
+            for x in range(len(matrice1) + 1):
+                aux_aux_func = A[f][x] * B[x][l]
+                aux_produtto_func = aux_aux_func + aux_produtto_func
+            matrix_produtto_func = np.append(matrix_produtto_func, aux_produtto_func)
+            aux_produtto_func = 0
+    matrix_produtto_func = matrix_produtto_func.reshape(2, 2)
+
+    return matrix_produtto_func
+
+
+
+a = produtto_matrice(A, B)
+print("\n", a)
+
+A1 = np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8]
+])
+B1 = np.array([
+    [1, 0, 2],
+    [0, 1, 3],
+    [1, 0, 0],
+    [2, 1, 1]
+])
+
+AB = produtto_matrice(A1, B1)
+print("Exemplo 1:\n", AB, "\n")
+
+
+A2 = np.array([
+    [-1, 0, 2, 1],
+    [3, -2, 0, 4]
+])
+B2 = np.array([
+    [2, -1, 0],
+    [1, 3, 2],
+    [0, 4, -2],
+    [-2, 1, 1]
+])
+
+AB1 = produtto_matrice(A2, B2)
+print("Exemplo 2:\n", AB1, "\n")
+
+
+A3 = np.array([
+    [0, 2, -1, 3],
+    [1, 1, 0, -2]
+])
+B3 = np.array([
+    [2, 1, 0],
+    [-1, 0, 2],
+    [0, -2, 1],
+    [1, 3, -1]
+])
+
+AB2 = produtto_matrice(A3, B3)
+print("Exemplo 3:\n", AB2, "\n")
+
+
+
